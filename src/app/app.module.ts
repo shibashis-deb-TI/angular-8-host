ca803838135a40f384a04ecb11613a4caed5a715
Shibashis Deb <shibashis.deb@technoidentity.com>
2024-05-20 16:18:26 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { UserComponent } from "./user/user.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class AppModule {}
