628506acd296bab1cd51b5151e96955185ec5db8
Shibashis Deb <shibashis.deb@technoidentity.com>
2024-05-20 17:38:58 +0530

    consume http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
+import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from "@angular/platform-browser";
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { UserComponent } from "./user/user.component";
 
 @NgModule({
   declarations: [AppComponent],
